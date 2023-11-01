5352f2d8fce82065cbc53de99bf69ca1766312e8
Yasufumi Sakai <89558631+YasufumiSakai@users.noreply.github.com>
2023-11-01 23:36:25 +0900

    Update run.sh

--- examples/resnet110_cifar10/run.sh
+++ examples/resnet110_cifar10/run.sh
@@ -1,1 +1,2 @@
+# COPYRIGHT Fujitsu Limited 2023
 CUDA_VISIBLE_DEVICES='0' python3 main.py --data ../data --pretrained_model_path ../pretrained_model/pretrained_cifar10_resnet110.pt --use_gpu --use_DataParallel > log.log
